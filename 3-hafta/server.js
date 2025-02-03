3af9d90b8f58091ea498a6fc3766941c849828ec
Anıl Tarar <anil_tarar@hotmail.com>
2025-02-03 16:23:01 +0300

    Ekstra Özellikler Eklendi

--- 3-hafta/server.js
+++ 3-hafta/server.js
@@ -16,12 +16,62 @@ app.get("/", (req, res) => {
 // Kitapları getiren fonksiyon
 const readBooks = () => {
   const jsonBooks = fs.readFileSync(booksPath);
+  if (jsonBooks.length === 0) {
+    return [];
+  }
   return JSON.parse(jsonBooks);
 };
-// Get isteği
+
+// Get isteği ->  Query Almadan Hepsini Çekiyoruz
+
+// app.get("/books", (req, res) => {
+//   const books = readBooks();
+//   res.status(200).send(books);
+// });
+
 app.get("/books", (req, res) => {
-  const books = readBooks();
-  res.status(200).send(books);
+  // const genre = req.query.genre; // Buradan genre bilgisini alıyoruz.
+  // const year = req.query.year; // Buradan year bilgisini alıyoruz.
+  // const page = req.query.page; // Buradan page bilgisini alıyoruz.
+  // const limit = req.query.limit; // Buradan limit bilgis
+  const {genre, year, page, limit} = req.query; // Destructuring ile aynı işlemi yapıyoruz.
+
+  const books = readBooks(); // Tüm kitapları alıyoruz.
+
+  if (year) {
+    const filteredBooks = books.filter((book) => book.year === parseInt(year));
+    if (filteredBooks.length === 0) {
+      return res.status(404).send("Kitap bulunamadı.");
+    }
+    return res.status(200).send(filteredBooks);
+  }
+
+
+  if (genre) {
+    const filteredBooks = books.filter(
+      (book) => book.genre.toLowerCase() === genre.toLowerCase()
+    ); // Eğer genre varsa filtreleme yapıyoruz.
+
+    if (filteredBooks.length === 0) {
+      return res.status(404).send("Kitap bulunamadı.");
+    } // Eğer filtreleme sonucunda hiç kitap bulunamazsa 404 dönüyoruz.
+
+    return res.status(200).send(filteredBooks);
+  }
+
+
+  if (page && limit) {
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const paginatedBooks = books.slice(startIndex, endIndex);
+    if (paginatedBooks.length === 0) {
+      return res.status(404).send("Kitap bulunamadı.");
+    }
+    return res.status(200).send(paginatedBooks);
+  }
+
+
+  res.status(200).send(books); // Eğer genre yoksa tüm kitapları gönderiyoruz.
 });
 
 // Yeni Kitap Ekle **************** POST BOOK
@@ -81,9 +131,8 @@ const isBookExist = (req, res, next) => {
 app.delete("/books/:id", isBookExist, (req, res) => {
   const { id } = req.params;
   deleteBook(parseInt(id));
-  res.status(204).send("Kitap silindi.");
+  return res.status(200).send("Kitap silindi.");
 });
-
 
 // Kitap Güncelleme **************** PUT BOOK
 
@@ -91,7 +140,15 @@ app.delete("/books/:id", isBookExist, (req, res) => {
 const updateBook = (id, updatedBook) => {
   let books = readBooks();
   const bookIndex = books.findIndex((book) => book.id === id);
-  books[bookIndex] = { id, ...updatedBook };
+  const oldBook = books[bookIndex];
+  const newBook = {
+    title: updatedBook.title || oldBook.title,
+    author: updatedBook.author || oldBook.author,
+    genre: updatedBook.genre || oldBook.genre,
+    year: updatedBook.year || oldBook.year,
+    pages: updatedBook.pages || oldBook.pages,
+  };
+  books[bookIndex] = { id, ...newBook };
   setBooks(books);
 };
 
@@ -103,23 +160,18 @@ app.put("/books/:id", isBookExist, (req, res) => {
   res.status(200).send("Kitap güncellendi.");
 });
 
-
 // Kitap Detayı **************** GET BOOK
 
 // HTTP GET isteği ile kitap detayı
 
 app.get("/books/:id", isBookExist, (req, res) => {
-    const id = parseInt(req.params.id);
-    const books = readBooks();
-    const book = books.find(book => book.id === id);
-    if(book){
-        res.status(200).send(book);
-    }
+  const id = parseInt(req.params.id);
+  const books = readBooks();
+  const book = books.find((book) => book.id === id);
+  if (book) {
+    res.status(200).send(book);
+  }
 });
-
-
-
-
 
 // Express üzerinden serveri ayağa kaldırdık.
 const PORT = 3000;
